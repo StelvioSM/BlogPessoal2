b2c7375e04eac1f6d3540940eaeb2ca09b25050f
Stelvio Alexandre Smith <silviosmith18@gmail.com>
2021-09-06 13:40:14 -0300

    Implementacao put delete tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -20,8 +20,21 @@ export class TemaService {
     return this.http.get<Tema[]>("http://localhost:8080//api/v1/tema", this.token )
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>("http://localhost:8080//api/v1/tema/${id}", this.token)
+
+  }
+
   posTema(tema:Tema): Observable<Tema>{
     return this.http.post<Tema>("http://localhost:8080//api/v1/tema",tema, this.token)
 
   }
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>("http://localhost:8080//api/v1/tema",tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete("http://localhost:8080//api/v1/tema/${id}", this.token)
+  }
+
 }
